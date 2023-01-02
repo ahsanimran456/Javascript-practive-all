987fbe671dc83d810420cf3ec10268a438a42881
Ahsan Imran <111527387+ahsanimran456@users.noreply.github.com>
2023-01-03 00:09:47 +0500

    Add files via upload

--- Array pratice/app.js
+++ Array pratice/app.js
@@ -1,14 +1,30 @@
 const Adduser_btn = document.getElementById("Adduser")
+const eachuserlist = document.getElementById("eachuserlist")
+
  let users = []
+ let count = 0 
  const adduser = async()=>{
     const url = await fetch('https://jsonplaceholder.typicode.com/users')
      const data = await url.json()
-     console.log(data)
-     const userlist = data[0].name
+    //  console.log(data)
+     count++ 
+     const userlist = data[count].name
      const userCurrent = {
         name : userlist,
         money : Math.floor(Math.random() * 10000)
 
      }
      console.log(userCurrent)
+     adddata_array(userCurrent)
+     eachuserlist.innerHTML+=`
+        <div class="eachuser">
+            <p><span>Name:${userCurrent.name}</span> </p>
+        </div>
+     `
  }
+
+ const adddata_array = (listofpep)=>{
+    users.push(listofpep)
+    console.log(users)
+ }
+ 
