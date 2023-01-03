1af8cd2a49fc9f50fc851f27c303f77946d9cfe8
Ahsan Imran <111527387+ahsanimran456@users.noreply.github.com>
2023-01-03 22:22:19 +0500

    Add files via upload
    get & delete users from list

--- Array pratice/app.js
+++ Array pratice/app.js
@@ -1,30 +1,48 @@
 const Adduser_btn = document.getElementById("Adduser")
 const eachuserlist = document.getElementById("eachuserlist")
 
- let users = []
- let count = 0 
- const adduser = async()=>{
-    const url = await fetch('https://jsonplaceholder.typicode.com/users')
-     const data = await url.json()
-    //  console.log(data)
-     count++ 
-     const userlist = data[count].name
-     const userCurrent = {
-        name : userlist,
-        money : Math.floor(Math.random() * 10000)
-
-     }
-     console.log(userCurrent)
-     adddata_array(userCurrent)
-     eachuserlist.innerHTML+=`
+let users = []
+let count = 0
+const adduser = async () => {
+   const url = await fetch('https://jsonplaceholder.typicode.com/users')
+   const data = await url.json()
+   //  console.log(data)
+   count++
+   const userlist = data[count].name
+   const userCurrent = {
+      name: userlist,
+      money: Math.floor(Math.random() * 100000 )
+   }
+   // console.log(userCurrent)
+   adddata_array(userCurrent)
+   eachuserlist.innerHTML += `
         <div class="eachuser">
-            <p><span>Name:${userCurrent.name}</span> </p>
+            <p class="names"><span>${userCurrent.name}</span> </p>
+            <p class="money"><span> ${userCurrent.money}$</span> </p>
+
         </div>
      `
- }
+}
 
- const adddata_array = (listofpep)=>{
-    users.push(listofpep)
-    console.log(users)
- }
- 
+const adddata_array = (listofpep) => {
+   users.push(listofpep)
+   console.log(users)
+}
+const Deleteuser = () => {
+   users.pop()
+   console.log(users)
+   eachuserlist.innerHTML = " ";
+      users.forEach(element => {
+         return (
+            eachuserlist.innerHTML += 
+               `<div class="eachuser">
+                     <p class="names"><span>${element.name}</span> </p>
+                     <p class="money"><span>${element.money}</span> </p>
+                </div>`
+         )
+      });
+}
+
+
+
+
